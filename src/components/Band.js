31799a9919f994b2ff9f12249ee10321e55ccece
sudimac <vojvoda1988@gmail.com>
2022-08-04 00:49:08 +0200

    nastavak1

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "../css/band.css";
 
 function Band({ band }) {
-  console.log(band);
-  return <div>radi</div>;
+  return (
+    <div className="band">
+      <div className="hed">
+        <h2>naslov</h2>
+      </div>
+      <div className="body">
+        <img src="#" alt="img" />
+        <p>text</p>
+      </div>
+      <div className="fut">
+        <button>Read more</button>
+      </div>
+    </div>
+  );
 }
 
 export default Band;
